2d20aca9e92e28122303735bc873bd514146ff95
rakesh <peramrakesh2000@gmail.com>
2025-01-27 16:34:34 +0530

    aa

--- generate_har.py
+++ generate_har.py
@@ -2,7 +2,7 @@ import json
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Optionsgit
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
